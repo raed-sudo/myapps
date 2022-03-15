6c1bae3e0c223e8ce7ecb709101ae9a28e394f28
raed-sudo <raed.louati@hotmail.fr>
2022-03-15 14:06:50 +0100

    upt

--- article.py
+++ article.py
@@ -2,23 +2,41 @@ from configparser import MAX_INTERPOLATION_DEPTH
 
 
 class Article():
-    matiere=''
     def __init__(self,name)-> None:
         self.name=name
 
-    def get_matiere(self) -> str:
-        return self.matiere
-
-    def set_matiere(self,matiere) -> None:
-        self.matiere=matiere
-
-    def set_weight(self,weight:float) -> None:
-        self.weight=weight
-
-    def get_weight(self) -> float:
-        return self.weight
+        
+    @property
+    def name(self):
+        return self.__name 
+    @name.setter
+    def name(self,value):
+        #cool validation here
+        if value != '':
+            self.__name = value
+        else:
+            print('Name cannot be NULL')
+    
+    @property
+    def matiere(self):
+        return self.__matiere
+    @matiere.setter
+    def matiere(self,value:str):
+        print('In the setter of matiere')
+        self.__matiere = value
+        
+    @property
+    def weight(self):
+        return self.__weight 
+    @weight.setter
+    def weight(self,value:float):
+        print('In the setter of weight')
+        self.__weight = value  
 
     def display_article(self) :
         return f"Article Name : {self.name} \
             , Matiere fabricatiob : {self.matiere} \
             , article weight: {self.weight}"
+
+    def __recondition_article(self):
+        return 'article reconditioned'
