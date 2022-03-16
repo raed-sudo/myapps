eb4ba6dbda7f7d2f02b7964a55b1a8e43c695b45
raed-sudo <raed.louati@hotmail.fr>
2022-03-16 23:39:31 +0100

    update

--- radus.py
+++ radus.py
@@ -3,3 +3,7 @@ from math import pi
 
 def circle_area(r):
     return pi*(r**2)
+
+def circle_perimetre(r):
+    return 2*pi*r
+
--- test_radus.py
+++ test_radus.py
@@ -1,6 +1,7 @@
+from calendar import c
 import unittest
 from math import pi
-from radus import circle_area
+from radus import circle_area, circle_perimetre
 
 class TestCircleArea(unittest.TestCase):
     def test_area(self):
@@ -8,5 +9,7 @@ class TestCircleArea(unittest.TestCase):
         self.assertEqual(circle_area(1),pi)
         self.assertRaises(ValueError,circle_area, -2)
     
-
-unittest.main()
+    def test_CirclePerimetre(self):
+        # Test Perimetre when radius >= 0
+        self.assertEqual(circle_perimetre(1),pi*2)
+        self.assertRaises(ValueError,circle_perimetre, -22)
