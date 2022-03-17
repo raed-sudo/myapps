caae3bf8a09c7636a67ae39ca56950b57a3befc3
raed-sudo <raed.louati@hotmail.fr>
2022-03-17 18:24:46 +0100

    update

--- radus.py
+++ radus.py
@@ -2,8 +2,17 @@
 from math import pi
 
 def circle_area(r):
+    if r <0 : 
+        raise(ValueError)
     return pi*(r**2)
 
 def circle_perimetre(r):
+    if isinstance(r,str):
+        raise(TypeError)
+    if r < 0  : 
+        raise(ValueError)
+
+
+
     return 2*pi*r
 
